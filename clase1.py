252fcd035e16c0fe693250db59dae81c61b87248
Jeremias <135482686+Jere3200@users.noreply.github.com>
2024-09-19 20:13:43 -0300

    Add files via upload

---
+++ clase1.py
@@ -0,0 +1,21 @@
+# 1) Crea un código que imprima en pantalla la expresión 'Me encanta estudiar Python '
+#print('Me encanta estudiar Python')
+# 2) Crea un codigo que imprima la expresion 'Estudiar con "Python Total" es super divertido '
+#print( "Estudiar con \"Pyton total\" es super divertido")
+# 3) Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente, sino como resultado de una operación matemática
+#sum= 155+400
+#print(sum)
+# 4) Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta: ¿Qué estás estudiando? El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+#preg=input("¿Qué estás estudiando?: ")
+#print ("El alumno esta estudiando:", preg)
+# 5) Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta: ¿En qué país vives?
+#preg=input("¿En qué país vives?: ")
+#print ("El usuario vive en:", preg)
+# 6) Crea un código que muestre en pantalla el nombre completo del usuario, permitiéndole ingresar su nombre y apellido con las siguientes instrucciones:
+# El código debe poder imprimir en pantalla el nombre y apellido del usuario, separados por un espacio.
+nom=input("Escribe tu nombre: ")
+apel=input("Escribe tu apellido: ")
+print("Tu nombre  y apellido es: ",nom,apel)
+
+
+
---
+++ prueba.py
@@ -0,0 +1,16 @@
+from statistics import median
+
+print("Ingresa 3 números disferentes para calcular el mayor")
+
+n1=int(input("Ingresa el primer numero:"))
+n2=int(input("Segundo el primer numero:"))
+n3=int(input("Tercer el primer numero: "))
+
+maxvalor= max(n1,n2,n3)
+minvalor= min(n1,n2,n3)
+
+print("El numero de mayor valor es: ", maxvalor)
+print("El numero de menor valor es: ", minvalor)
+
+
+
